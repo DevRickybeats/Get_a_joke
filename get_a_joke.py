3b852699d3b3aaf2f244f5a217e20054cec9a098
IGWE RICKSON IKENNA <114086809+DevRickybeats@users.noreply.github.com>
2022-11-26 11:34:16 +0000

    Add files via upload

--- get_a_joke.py
+++ get_a_joke.py
@@ -1,9 +1,22 @@
 import requests
-tut = input('What would you like to search for? ')
+from random import choice
+
+user_input = input('What would you like to search for? ')
 url = 'https://icanhazdadjoke.com/search'
 res = requests.get(url,
                    headers={'accept': 'application/json'},
-                   params={'term': tut}
+                   params={'term': user_input}
                    ).json()
 
-print(res)
+# print(res['results'])
+result = res['results']
+# print(len(result))
+num_jokes = res['total_jokes']
+if num_jokes > 1:
+    print(f'''I found {num_jokes} jokes about {user_input}, Here's one: ''')
+    print(choice(result)['joke'])
+elif num_jokes == 1:
+    print('There is one joke')
+    print(choice(result)['joke'])
+else:
+    print(f'There are no jokes with {user_input} in it')
